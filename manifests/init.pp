7157b5a4a5f38ff9251e68820accb9eaec66074a
Lorenzo Cocchi <lorenzo.cocchi@softecspa.it>
2014-11-21 16:55:23 +0100

    start NFS disposal

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 class softec_kvm (
-  $extra_packages = params_lookup( 'extra_packages' ),
-
+  $extra_packages          = params_lookup( 'extra_packages' ),
+  $enable_softec_guest_xml = params_lookup( 'enable_softec_guest_xml' ),
 ) inherits softec_kvm::params{
   include kvm
 
@@ -9,9 +9,9 @@ class softec_kvm (
   }
 
   file { '/usr/share/kvmclone':
-    source  =>  'puppet:///modules/softec_kvm/share/kvmclone',
     ensure  =>  directory,
-    mode    =>  0755,
+    source  =>  'puppet:///modules/softec_kvm/share/kvmclone',
+    mode    =>  '0755',
     recurse =>  true,
     ignore  =>  '.pyc',
     owner   =>  'root',
@@ -19,9 +19,39 @@ class softec_kvm (
   }
 
   file { '/usr/local/bin/guestclone':
-    ensure    =>  'link',
-    target    =>  '/usr/share/kvmclone/guestclone.py',
-    require   =>  [ File['/usr/share/kvmclone'], ],
+    ensure  =>  'link',
+    target  =>  '/usr/share/kvmclone/guestclone.py',
+    require =>  [ File['/usr/share/kvmclone'], ],
   }
 
+  if $enable_softec_guest_xml {
+    nfs::mount { '/mnt/backup_vm':
+      ensure         => absent,
+      server         => 'str-vdisk.tetsuya.backplane',
+      share          => '/backup_manipolo',
+      client_options => $nfs_mount_options,
+    } ->
+
+    file { '/etc/libvirt/qemu/lucid.softecspa.it.xml':
+      ensure => present,
+      source => 'puppet:///modules/softec_private/etc/libvirt/qemu/lucid.softecspa.it.xml',
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0600',
+      notify => Service['libvirt-bin'],
+    } ->
+
+    file { '/etc/libvirt/qemu/precise.softecspa.it.xml':
+      ensure => present,
+      source => 'puppet:///modules/softec_private/etc/libvirt/qemu/precise.softecspa.it.xml',
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0600',
+      notify => Service['libvirt-bin'],
+    }
+
+    service { 'libvirt-bin':
+      ensure => running
+    }
+  }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,13 +1,23 @@
 class softec_kvm::params {
+
   case $::operatingsystem {
     'ubuntu', 'debian': {
-      $extra_packages = ['ubuntu-virt-server', 'virtinst', 'vlan',
-                         'cpu-checker', 'libguestfs-tools', 'kpartx',
-                         'virt-top', 'guestmount',]
+      $extra_packages = [
+        'ubuntu-virt-server',
+        'virtinst',
+        'vlan',
+        'cpu-checker',
+        'libguestfs-tools',
+        'kpartx',
+        'virt-top',
+        'guestmount',
+      ]
       $acpid_package  = 'acpid'
     }
     default : {
       fail("Operating system ${::operatingsystem} is not supported")
     }
   }
+
+  $enable_softec_guest_xml = false
 }
